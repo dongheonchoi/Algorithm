ac08a2f2258bfe876fd17b3ed02e779ca57aac86
dongheonchoi <124031425+dongheonchoi@users.noreply.github.com>
2023-11-30 19:00:09 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.06 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public long solution(long a, long b) {
+        long answer = ((Math.abs(a-b)+1)*(a+b))/2;
+        
+        
+        
+        return answer;
+    }
+}
