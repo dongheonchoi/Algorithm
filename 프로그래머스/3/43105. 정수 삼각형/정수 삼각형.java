2ae5b66032504d2e510b54de1099cb2dacf2cbc4
dongheonchoi <124031425+dongheonchoi@users.noreply.github.com>
2023-12-17 21:53:25 +0900

    [level 3] Title: 정수 삼각형, Time: 6.42 ms, Memory: 61.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] triangle) {
+        
+        for(int i = triangle.length-1; i >= 1; i--){
+            
+            for(int j = 0; j < triangle[i].length-1; j++){
+                triangle[i-1][j] +=  Math.max(triangle[i][j] , triangle[i][j+1]);
+            }
+            
+        }
+        
+        
+        
+        
+        return triangle[0][0];
+    }
+}
