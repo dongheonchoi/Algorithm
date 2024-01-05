4f64786d9aca61026d1282953031685dc94c5bf7
dongheonchoi <124031425+dongheonchoi@users.noreply.github.com>
2024-01-05 20:18:50 +0900

    [level unrated] Title: 주사위 게임 2, Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int a, int b, int c) {
+        
+        if(a == b ){
+            if(b == c){
+                return (a+b+c)*(a*a+b*b+c*c)*(a*a*a+b*b*b+c*c*c);
+            }else{
+                return (a+b+c)*(a*a+b*b+c*c);
+            }
+        }else if(b == c){
+             return (a+b+c)*(a*a+b*b+c*c);
+        }else if(a == c){
+             return (a+b+c)*(a*a+b*b+c*c);
+        }else{
+            return a + b + c;
+        }
+
+    }
+}
